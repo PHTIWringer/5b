ed1f67e919faadf2e991cd0bc925b1cd61767457
Ken Hileman <ken.hileman@outlook.com>
2024-04-26 19:42:56 -0400

    assignment 5b

---
+++ taxicab.py
@@ -0,0 +1,34 @@
+# Author: Kenneth Hileman
+# GitHub username: PHTIWringer
+# NOTE: Will not have a ReadMe - Using another IDE
+# Date: 04/26/2024
+# Description: Program that tracks how far a taxicab has moved via an odometer and also how far it has moved up/down and left/right.
+
+class Taxicab:
+    def __init__(self, x_init, y_init):
+        '''Creates the Taxicab class with x and y coordinates plus the odometer which tracks total movements'''
+        self.x_coord = x_init
+        self.y_coord = y_init
+        self.odometer = 0
+
+    def get_x_coord(self):
+        '''Return the x coordinate'''
+        return self.x_coord
+    
+    def get_y_coord(self):
+        '''Return the y coordinate'''
+        return self.y_coord
+    
+    def get_odometer(self):
+        '''Return odometer reading'''
+        return self.odometer
+    
+    def move_x(self, movement):
+        '''Moves taxi on the x-axis and adds total x movements to the odometer'''
+        self.x_coord += movement
+        self.odometer += abs(movement)
+
+    def move_y(self, movement):
+        '''Moves taxi on the y-axis and adds total y movements to the odometer'''
+        self.y_coord += movement
+        self.odometer += abs(movement)
